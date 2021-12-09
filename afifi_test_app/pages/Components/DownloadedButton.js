0f729dfc3ae2fb75d34b143b8b88d7454fcd246e
Afifi Jamal <afifi.jamal@fdmgroup.com>
2021-12-09 10:37:28 +0800

    Initial commit

---
+++ afifi_test_app/pages/Components/DownloadedButton.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { useQuery } from '@apollo/react-hooks';
+import { Button } from '@shopify/polaris';
+import gql from 'graphql-tag';
+import store from 'store-js';
+import { generateExcelFile } from '../GenerateExcel';
+
+const GET_VALUES_BY_IDS = gql`
+query getProducts($ids: [ID!]!) {
+  nodes(ids: $ids) {
+    ... on Product {
+      title
+      totalVariants
+      variants(first: 10){
+        edges {
+          node {
+            sku
+            inventoryQuantity
+            price
+            title
+          }
+        }
+      }
+    }
+  }
+}
+`;
+
+function DownloadedButton() {
+      console.log("In downloaded button")
+        var tempMap = new Map();
+        const {error, loading, data} = useQuery(GET_VALUES_BY_IDS, {
+        variables: { ids: store.get('ids') }
+        });
+        if(error){
+            console.log("Error when querying")
+            console.log(error)
+        }
+        if(data){
+            data.nodes.forEach(node => {
+            //Process each product here
+            if(node.totalVariants !== 0){
+                var variants = node.variants.edges;
+                tempMap.set(node.title, variants)
+            }
+            })
+            generateExcelFile(tempMap)
+        }
+    return (
+        <Button>File Downloaded</Button>
+    )
+}
+
+export default DownloadedButton
