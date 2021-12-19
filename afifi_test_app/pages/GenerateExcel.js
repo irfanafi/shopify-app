e5b5305af207a94e3056c89267cbe719cf6556f8
Afifi Jamal <afifi.jamal@fdmgroup.com>
2021-12-20 00:15:32 +0800

    add

--- afifi_test_app/pages/GenerateExcel.js
+++ afifi_test_app/pages/GenerateExcel.js
@@ -1,69 +1,64 @@
-import * as XLSX from 'xlsx';
+import * as XLSX from "xlsx";
 
-export function generateExcelFile(map, filters){
-    var wb = XLSX.utils.book_new();
-    // For each product, fill up the sheet
-    map.forEach((values,keys)=>{
-        let titles = [''];
-        let skuArr = [''];
-        let qtyArr = [''];
-        //For each variant, populate accordingly
-        values.forEach(value => {
-            if(value.node.inventoryQuantity !== 0){
-                titles.push(value.node.title);
-                titles.push(" ");
-                if(value.node.sku !== ''){
-                    skuArr.push(value.node.sku);
-                    skuArr.push(" ");
-                }
-                if(value.node.inventoryQuantity !== 0){
-                    qtyArr.push(value.node.inventoryQuantity);
-                    qtyArr.push(" ");
-                }
-               
-            }
-        })
-        let arrOfValues = [];
-        arrOfValues.push(titles);
-        
-        
-        if(skuArr.length !== 1){
-            arrOfValues.push(skuArr);
+export default function generateExcelFile(map, filters) {
+  var wb = XLSX.utils.book_new();
+  // For each product, fill up the sheet
+  map.forEach((values, keys) => {
+    let titles = [""];
+    let skuArr = [""];
+    let qtyArr = [""];
+    //For each variant, populate accordingly
+    values.forEach((value) => {
+      if (value.node.inventoryQuantity !== 0) {
+        titles.push(value.node.title);
+        titles.push(" ");
+        if (value.node.sku !== "") {
+          skuArr.push(value.node.sku);
+          skuArr.push(" ");
         }
-        if(qtyArr.length !== 1){
-            arrOfValues.push(qtyArr);
+        if (value.node.inventoryQuantity !== 0) {
+          qtyArr.push(value.node.inventoryQuantity);
+          qtyArr.push(" ");
         }
-        if(arrOfValues.length > 1){
-            let tempValue = generateColumnNumbering(qtyArr)
-            let newArr = arrOfValues.concat(tempValue)
-            var ws = XLSX.utils.aoa_to_sheet(newArr);
-            XLSX.utils.book_append_sheet(wb, ws, keys);
-        }
-        
-    })
-    XLSX.writeFile(wb, "GenerateExcelFile.xlsx");
+      }
+    });
+    let arrOfValues = [];
+    arrOfValues.push(titles);
+
+    if (skuArr.length !== 1) {
+      arrOfValues.push(skuArr);
+    }
+    if (qtyArr.length !== 1) {
+      arrOfValues.push(qtyArr);
+    }
+    if (arrOfValues.length > 1) {
+      let tempValue = generateColumnNumbering(qtyArr);
+      let newArr = arrOfValues.concat(tempValue);
+      var ws = XLSX.utils.aoa_to_sheet(newArr);
+      XLSX.utils.book_append_sheet(wb, ws, keys);
+    }
+  });
+  XLSX.writeFile(wb, "GenerateExcelFile.xlsx");
 }
 
-function generateColumnNumbering(quantityArray){
-    let tempArr = [];
-    let u = 1;
-    //Get max value from the array
-    let maxQty = Math.max.apply(Math,quantityArray);
-    while(u<=maxQty){
-        let tempArr1 = [];
-        for (var i = 1; i < quantityArray.length; i = i+2) {
-            if(quantityArray[i] >= u){
-                tempArr1.push(u);
-            }
-            else{
-                tempArr1.push(' ');
-            }
-            tempArr1.push(' ');
-        }
-        //After row 1 is done, push the row to Array
-        tempArr.push(tempArr1);
-        u = u+1;
+function generateColumnNumbering(quantityArray) {
+  let tempArr = [];
+  let u = 1;
+  //Get max value from the array
+  let maxQty = Math.max.apply(Math, quantityArray);
+  while (u <= maxQty) {
+    let tempArr1 = [];
+    for (var i = 1; i < quantityArray.length; i = i + 2) {
+      if (quantityArray[i] >= u) {
+        tempArr1.push(u);
+      } else {
+        tempArr1.push(" ");
+      }
+      tempArr1.push(" ");
     }
-    return tempArr;
-    
+    //After row 1 is done, push the row to Array
+    tempArr.push(tempArr1);
+    u = u + 1;
+  }
+  return tempArr;
 }
